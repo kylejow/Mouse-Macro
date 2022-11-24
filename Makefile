121b0df6529d92648f1b02d091ce77ea5fb394ec
kylejow <77653395+kylejow@users.noreply.github.com>
2022-11-23 19:18:32 -0800

    test keyboard

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CXXFLAGS=-Wall -g
 CXX=g++
 main: main.o display.o screen.o mouse.o cputimer.o menu.o program.o
 	$(CXX) -o $@ $^
-keyboardActions: keyboardActions.o
+keyboardActions: keyboardActions.o program.o
 	$(CXX) -o $@ $^
 clean:
 	rm -f *.o *.exe
--- keyboardActions.cpp
+++ keyboardActions.cpp
@@ -7,8 +7,12 @@
 #include <thread>
 #include <chrono>
 
+#include "program.h"
+
 using std::vector;
 using std::cout;
+using std::thread;
+using std::ref;
 
 vector<bool> keysDown(void){
     vector<bool> keys(256);
@@ -25,19 +29,42 @@ void recordKeyboard(std::atomic_bool& stop, vector<vector<bool>>& keys){
     }
     return;
 }
+void keyPress(int duration, int key){
+    INPUT down[1] = {0};
+    down[0].type = INPUT_KEYBOARD;
+    down[0].ki.wVk= key;
+    SendInput(1, down, sizeof(INPUT));
+    std::this_thread::sleep_for(std::chrono::milliseconds(duration));
+    INPUT up[1] = {0};
+    up[0].type = INPUT_KEYBOARD;
+    up[0].ki.wVk= key;
+    up[0].mi.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, up, sizeof(INPUT));
+}
 void runKeyboard(vector<vector<bool>>& keys){
     vector<bool> prev(256, false);
     for(auto iter = keys.begin(); iter != keys.end(); iter++){
-        for(auto iter2 = (*iter).begin(); iter2 != (*iter).end(); iter2++){
-
+        for(int i = 0; i < 256; i++){
+            if((*iter)[i] == true){
+                keyPress(0, i);
+            }
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 }
+
 int main(){
-    Sleep(2000);
-    vector<bool> keys = keysDown();
-    if(keys[0x38]){
-        cout << "8 down";
-    }
+    system("cls");
+    cout << "\nPress shift to start recording\n";
+    while(!(GetKeyState(VK_SHIFT) & 0x8000)){};
+    std::atomic_bool stop = false;
+    vector<vector<bool>> keys;
+    thread stopThread(stopProgram, ref(stop));
+    thread sdjifndas(recordKeyboard, ref(stop), ref(keys));
+    sdjifndas.join();
+    stopThread.join();
+    system("cls");
+    cout << "\nPress shift to run\n";
+    while(!(GetKeyState(VK_SHIFT) & 0x8000)){};
+    runKeyboard(keys);
 }
